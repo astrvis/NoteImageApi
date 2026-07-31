ed9bef580308581042d6f94f56ae632e9f6414ea
astrvis <astrvis@qq.com>
2026-08-01 06:50:43 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { serve } from "@hono/node-server"
 import { app } from "./routes/app.js"
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`)
-  },
-)
+const IS_VERCEL = process.env.VERCEL === "1"
+const IS_DEV = process.env.NODE_ENV !== "production"
+
+if (!IS_VERCEL) {
+  serve(
+    {
+      fetch: app.fetch,
+      port: Number(process.env.PORT ?? 3000),
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`)
+    },
+  )
+}
+
 export default app
